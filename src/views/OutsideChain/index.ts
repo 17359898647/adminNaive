707e85e91822ff6bdec23a801b8442d888733ac0
黄洋洋 <1635656638@qq.com>
2023-09-19 23:03:39 +0800

    fix: 🧩 修复菜单

--- src/views/OutsideChain/index.ts
+++ src/views/OutsideChain/index.ts
@@ -1,7 +1,7 @@
+import type { RouteMeta } from 'vue-router/auto'
+
 export default {
-  '/OutsideChain': {
-    isTitle: '外链',
-    lineIcon: 'twemoji:couple-with-heart-man-man-light-skin-tone-dark-skin-tone',
-    isOrder: Number.POSITIVE_INFINITY,
-  },
-} as IEnhanceAutoRouter
+  isTitle: '外链',
+  lineIcon: 'twemoji:couple-with-heart-man-man-light-skin-tone-dark-skin-tone',
+  isOrder: Number.POSITIVE_INFINITY,
+} as RouteMeta
