e871300093ceb1e1fd783ca99aa56cd013cbb35e
黄洋洋 <1635656638@qq.com>
2023-09-19 08:37:37 +0800

    feat: 🚀 在线预览

---
+++ src/views/OutsideChain/LineNet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePage({
+  name: 'https://17359898647.github.io/',
+  meta: {
+    isTitle: '在线预览',
+    lineIcon: 'icon-park:preview-open',
+    isOrder: Number.POSITIVE_INFINITY,
+  },
+})
+</script>
+
+<template>
+  naive ui
+</template>
