37e1513c38cd0b8031ecaeca270bc529ede28fc6
黄洋洋 <1635656638@qq.com>
2023-09-21 11:32:11 +0800

    fix: 🧩 在线预览打开新窗口

--- src/views/OutsideChain/LineNet.vue
+++ src/views/OutsideChain/LineNet.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 definePage({
+  name:'https://17359898647.github.io/',
   meta: {
     isTitle: '在线预览',
     lineIcon: 'icon-park:preview-open',
     isOrder: Number.POSITIVE_INFINITY,
-    isIframe: true,
   },
 })
 </script>
