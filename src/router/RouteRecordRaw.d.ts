7a9ca977c739fe5e0150129db69a51e68e9a1221
黄洋洋 <1635656638@qq.com>
2023-09-25 16:56:18 +0800

    fix: 🧩 routeMeta类型统一

--- src/router/RouteRecordRaw.d.ts
+++ src/router/RouteRecordRaw.d.ts
@@ -2,42 +2,7 @@
 import 'vue-router'
 
 declare module 'vue-router' {
-  interface RouteMeta {
-    /** 路由标题 */
-    isTitle: string
-    /**
-     * 是否改变网页标题
-     * @default true
-     */
-    changeTitle?: boolean
-    /**
-     * 菜单顺序,同层比较
-     * @default 0
-     */
-    isOrder?: number
-    /** 图标名称 */
-    lineIcon?: string
-    /** 本地图标名称 */
-    localIcon?: string
-    /**
-     * 是否是固定页，默认不固定
-     * @default undefined
-     */
-    isAffix?: true
-    /**
-     * 是否隐藏在菜单,面包屑中隐藏，默认不隐藏
-     * @default undefined
-     */
-    isHidden?: true
-    /**
-     * 是否缓存,默认缓存
-     * @default undefined
-     */
-    isKeepAlive?: false
-    /**
-     * isIframe: 是否是iframe，默认不是
-     * @default undefined
-     */
-    isIframe?: true
+  interface RouteMeta extends routeMeta {
+
   }
 }
