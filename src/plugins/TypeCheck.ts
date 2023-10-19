bc0599596f85ff293913c79320e670dbc8ab4169
黄洋洋 <1635656638@qq.com>
2023-10-19 14:22:19 +0800

    fix: 🧩 vite-plugin-checker eslint 选项

--- src/plugins/TypeCheck.ts
+++ src/plugins/TypeCheck.ts
@@ -1,12 +1,18 @@
+/* prettier-ignore */
+// @ts-nocheck
 import process from 'node:process'
 import type { PluginOption } from 'vite'
-import checker from 'vite-plugin-checker'
+import checker from '@hyoban/vite-plugin-checker'
 
 export function TypeCheck(): PluginOption {
   return !process.env.VITEST
     ? checker({
       typescript: true,
       vueTsc: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,vue}"',
+        useFlatConfig: true,
+      },
     })
     : false
 }
